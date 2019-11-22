0d4d0e4dd09d3c7482c0c8b732556ed1b398e837
Sabry Razick <sabryr@gmail.com>
2019-11-22 08:02:55 +0100

    Change sh to bash

--- phred.sh
+++ phred.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 V="0.1"
 #sabryr  2019-11-21
 #To tanslate the Phred scores
 #Only one input character 
 
 SCORES="! \" # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] a b c d e f g e i j k l m n o p q r s t u v w x y z  { | } ~ "
-IFS=' ' #
+IFS=' '
 read -ra ADDR <<< $SCORES # str is read into an array as tokens separated by IFS
 POS=33
 M_POS=0
